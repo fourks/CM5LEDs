834234bd6edeb4dc1020de18e07cc7ab713d3644
William Dillon <william@housedillon.com>
2016-04-05 11:11:54 -0700

    Initial Commit

---
+++ CM5/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CM5
+//
+//  Created by William Dillon on 4/5/16.
+//  Copyright © 2016 housedillon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
